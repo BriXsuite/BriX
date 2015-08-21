61567005379260f0ed56bff323638ae7c4c3697e
cem <jimcc333@gmail.com>
2015-08-21 14:27:45 -0500

    part of previous commit

---
+++ src/librarytools.cc
@@ -0,0 +1,15 @@
+#include "librarytools.h"
+
+
+void LibraryReader(std::string library_path, LibInfo &library){
+    library.lib_BU = 5;
+
+    std::cout << "library reader!" << std::endl;
+
+}
+
+
+
+void test_funcc(int i){
+std::cout << "library reader! " << i << std::endl;
+}
---
+++ src/librarytools.h
@@ -0,0 +1,19 @@
+#ifndef LIBRARYTOOLS_H
+#define LIBRARYTOOLS_H
+
+#include <string>
+#include <sstream>
+#include <cmath>
+
+#include "structures.h"
+
+struct bla{
+    int one;
+    int two;
+};
+
+void LibraryReader(std::string library_path, LibInfo &library);
+
+void test_funcc(int i);
+
+#endif // LIBRARYTOOLS_H
